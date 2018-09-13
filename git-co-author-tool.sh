359097c1aeed9a11a052200b5e10cd7dda88cc5a
Abel Trotter <a_trotter0@hotmail.com>
2018-09-13 15:18:10 -0700

    Try echo -ne     Co-authored-by: Abel Trotter <a_trotter0@hotmail.com>     Co-authored-by: Ryan Murry <ryanjmurry@gmail.com>

--- git-co-author-tool.sh
+++ git-co-author-tool.sh
@@ -45,8 +45,8 @@ while true; do
     read msg
 
     git commit -m "$msg
-    yes
-    yes
+    echo -ne '\n'
+    echo -ne '\n'
     Co-authored-by: $user1FullName <$user1Email>
     Co-authored-by: $user2FullName <$user2Email>"
     
