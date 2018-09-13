85d813cd976973709760f7184baa9029273781d2
Abel Trotter <a_trotter0@hotmail.com>
2018-09-13 15:00:00 -0700

    Close while loop     Co-authored-by: Abel Trotter <a_trotter0@hotmail.com>     Co-authored-by: thad donaghue <thaddonaghue@yahoo.com>

--- git-co-author-tool.sh
+++ git-co-author-tool.sh
@@ -2,52 +2,52 @@
 # A Bash script to configure the co-author tool and create git commits using co-author.
 
 while true; do
+  echo MAIN MENU
+  echo ===========
+  echo Please select an option below:
+  echo 1. Set co-author config
+  echo 2. Create git commit
 
-echo MAIN MENU
-echo ===========
-echo Please select an option below:
-echo 1. Set co-author config
-echo 2. Create git commit
+  read input
 
-read input
+  # Option 1 - Set co-author config
+  if [ $input == 1 ]
+  then
+    # Get user1 data
+    echo Enter the first co-author full name.
+    echo example: Kevin Durant
+    read user1FullName
 
-# Option 1 - Set co-author config
-if [ $input == 1 ]
-then
-  # Get user1 data
-  echo Enter the first co-author full name.
-  echo example: Kevin Durant
-  read user1FullName
+    echo Enter the first co-author email.
+    echo example: kevindurant@basketballguy.com
+    read user1Email
 
-  echo Enter the first co-author email.
-  echo example: kevindurant@basketballguy.com
-  read user1Email
+    # Get user2 data
+    echo Enter the second co-author full name.
+    echo example: Kobe Bryant
+    read user2FullName
 
-  # Get user2 data
-  echo Enter the second co-author full name.
-  echo example: Kobe Bryant
-  read user2FullName
+    echo Enter the second co-author email.
+    echo example: kobebryant@basketballguy.com
+    read user2Email
 
-  echo Enter the second co-author email.
-  echo example: kobebryant@basketballguy.com
-  read user2Email
+    echo Co-author settings saved!
+  else
+    echo Please enter valid input.
+  fi
 
-  echo Co-author settings saved!
-else
-  echo Please enter valid input.
-fi
+  # Option 2 - Create git commit
+  if [ $input == 2 ]
+  then
+    # Prompt for commit message
+    echo Please enter a commit message:
+    read msg
 
-# Option 2 - Create git commit
-if [ $input == 2 ]
-then
-  # Prompt for commit message
-  echo Please enter a commit message:
-  read msg
-
-  git commit -m "$msg
-  Co-authored-by: $user1FullName <$user1Email>
-  Co-authored-by: $user2FullName <$user2Email>"
-  
-else
-  echo Please enter valid input.
-fi
+    git commit -m "$msg
+    Co-authored-by: $user1FullName <$user1Email>
+    Co-authored-by: $user2FullName <$user2Email>"
+    
+  else
+    echo Please enter valid input.
+  fi
+done
